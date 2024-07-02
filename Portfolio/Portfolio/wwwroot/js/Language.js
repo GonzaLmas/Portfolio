174bc2a1cb6410a0f3b522f659955992ca7d3940
GonzaLmas <gonzalolamas09@gmail.com>
2024-07-02 00:57:00 -0300

    Idioma inglés añadido. Actualización de CVs

---
+++ Portfolio/Portfolio/wwwroot/js/Language.js
@@ -0,0 +1,34 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var toggleButton = document.querySelector(".toggle-button");
+
+    function toggleVisibility() {
+        var isChecked = localStorage.getItem('languageToggle') === 'eng';
+
+        if (isChecked) {
+            $(".spa").hide();
+            $(".eng").show();
+            toggleButton.textContent = "ES";
+            toggleButton.classList.add('checked');
+        } else {
+            $(".eng").hide();
+            $(".spa").show();
+            toggleButton.textContent = "EN";
+            toggleButton.classList.remove('checked');
+        }
+    }
+
+    toggleVisibility();
+
+    toggleButton.addEventListener('click', function () {
+        toggleButton.classList.toggle('checked');
+        var isChecked = toggleButton.classList.contains('checked');
+
+        if (isChecked) {
+            localStorage.setItem('languageToggle', 'eng');
+        } else {
+            localStorage.setItem('languageToggle', 'spa');
+        }
+
+        toggleVisibility();
+    });
+});
