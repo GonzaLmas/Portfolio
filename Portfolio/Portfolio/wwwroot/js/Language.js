a903c31b3b88dbf727e6fa017d552949f665a1c0
GonzaLmas <gonzalolamas09@gmail.com>
2025-05-16 18:03:52 -0300

    Actualización pantalla "Resumen". Eliminación idioma ingles.

--- Portfolio/Portfolio/wwwroot/js/Language.js
+++ Portfolio/Portfolio/wwwroot/js/Language.js
@@ -1,34 +1,34 @@
-﻿document.addEventListener('DOMContentLoaded', function () {
-    var toggleButton = document.querySelector(".toggle-button");
+﻿//document.addEventListener('DOMContentLoaded', function () {
+//    var toggleButton = document.querySelector(".toggle-button");
 
-    function toggleVisibility() {
-        var isChecked = localStorage.getItem('languageToggle') === 'eng';
+//    function toggleVisibility() {
+//        var isChecked = localStorage.getItem('languageToggle') === 'eng';
 
-        if (isChecked) {
-            $(".spa").hide();
-            $(".eng").show();
-            toggleButton.textContent = "ES";
-            toggleButton.classList.add('checked');
-        } else {
-            $(".eng").hide();
-            $(".spa").show();
-            toggleButton.textContent = "EN";
-            toggleButton.classList.remove('checked');
-        }
-    }
+//        if (isChecked) {
+//            $(".spa").hide();
+//            $(".eng").show();
+//            toggleButton.textContent = "ES";
+//            toggleButton.classList.add('checked');
+//        } else {
+//            $(".eng").hide();
+//            $(".spa").show();
+//            toggleButton.textContent = "EN";
+//            toggleButton.classList.remove('checked');
+//        }
+//    }
 
-    toggleVisibility();
+//    toggleVisibility();
 
-    toggleButton.addEventListener('click', function () {
-        toggleButton.classList.toggle('checked');
-        var isChecked = toggleButton.classList.contains('checked');
+//    toggleButton.addEventListener('click', function () {
+//        toggleButton.classList.toggle('checked');
+//        var isChecked = toggleButton.classList.contains('checked');
 
-        if (isChecked) {
-            localStorage.setItem('languageToggle', 'eng');
-        } else {
-            localStorage.setItem('languageToggle', 'spa');
-        }
+//        if (isChecked) {
+//            localStorage.setItem('languageToggle', 'eng');
+//        } else {
+//            localStorage.setItem('languageToggle', 'spa');
+//        }
 
-        toggleVisibility();
-    });
-});
+//        toggleVisibility();
+//    });
+//});
